14107e1b223358caae092b40546c29ba53683d92
Shogo Sawai <shogo.sawai@gmail.com>
2022-02-08 15:16:16 +0900

    add example server

---
+++ example/server/server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Author {
+  id: Int!
+  firstName: String!
+  lastName: String!
+  posts(findTitle: String): [Post]
+}
+
+type Post {
+  id: Int!
+  title: String!
+  author: Author
+}
+
+enum SpanKind {
+  DAILY
+  MONTHLY
+}
+
+type Query {
+  hello: String
+  posts(slugList: [ID!], kind: SpanKind): [Post]
+}
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
